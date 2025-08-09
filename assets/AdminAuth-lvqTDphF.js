9c0922caddc614bbbe5435ad3c29b7efe6af623f
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-08-09 21:14:58 +0200

    dsfdsfdsf

---
+++ assets/AdminAuth-lvqTDphF.js
@@ -0,0 +1,1 @@
+import{A as e}from"./AuthWindow-DTQJ3Y_2.js";import{e as t,f as n,o}from"./index-CWN5NQHB.js";const d=t({__name:"AdminAuth",setup(i){return(m,c)=>(o(),n(e,{title:"Admin Interface",description:"Bitte melde dich mit deinem Admin Account an.",buttonText:"Anmelden"}))}});export{d as default};
