6757ec5046c8e0929d838a9023f43afc535f0292
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-08-09 21:09:22 +0200

    asdad

---
+++ assets/CreatorAuth-CBC6FcIb.js
@@ -0,0 +1,1 @@
+import{A as e}from"./AuthWindow-BeT8swwK.js";import{e as t,f as o,o as r}from"./index-D-1UNKQt.js";const s=t({__name:"CreatorAuth",setup(n){return(a,c)=>(r(),o(e,{title:"Creator Bereich",description:"Bitte melde dich mit deinem Creator Account an.",buttonText:"Anmelden"}))}});export{s as default};
