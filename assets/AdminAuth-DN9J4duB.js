fa8eec400997f7ea2bbf30d04dfd73a0d12f8173
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-09-10 20:34:20 +0200

    big update ALLA

---
+++ assets/AdminAuth-DN9J4duB.js
@@ -0,0 +1,1 @@
+import{A as e}from"./AuthWindow-CbYR-wQ6.js";import{d as t,a as n,o}from"./index-DnOdje8d.js";import"./InputGroup-OKeGS3xh.js";const p=t({__name:"AdminAuth",setup(i){return(m,r)=>(o(),n(e,{title:"Admin Interface",description:"Bitte melde dich mit deinem Admin Account an.",buttonText:"Anmelden"}))}});export{p as default};
---
+++ assets/CreatorAuth-Dj5iKWXJ.js
@@ -0,0 +1,1 @@
+import{A as e}from"./AuthWindow-CbYR-wQ6.js";import{d as t,a as o,o as r}from"./index-DnOdje8d.js";import"./InputGroup-OKeGS3xh.js";const s=t({__name:"CreatorAuth",setup(n){return(a,i)=>(r(),o(e,{title:"Creator Bereich",description:"Bitte melde dich mit deinem Creator Account an.",buttonText:"Anmelden"}))}});export{s as default};
